e0822a4d14cb43e32dcf44332721b48588e56beb
Robin <robinbreed92@gmail.com>
2023-04-18 12:36:29 +0200

    Still getting an error

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,43 +1,44 @@
 """
-Django Admin customizations.
+Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
-    ordering = ["id"]
-    list_display = ["email", "name"]
+    ordering = ['id']
+    list_display = ['email', 'name']
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
-            _("Permissions"),
+            _('Permissions'),
             {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
                 )
             }
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
-    readonly_fields = ["last_login"]
+    readonly_fields = ['last_login']
     add_fieldsets = (
         (None, {
-            "classes": ("wide",),
-            "fields": (
-                "email",
-                "password1",
-                "password2",
-                "name",
-                "is_active",
-                "is_staff",
-                "is_superuser",
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
             ),
         }),
     )
